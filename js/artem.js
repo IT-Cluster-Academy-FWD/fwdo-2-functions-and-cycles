7eaf668654d56061308c1e17fa041cff388533ad
deletko <deletko>
2020-08-31 22:49:07 +0300

    after lesson

---
+++ js/artem.js
@@ -0,0 +1,13 @@
+let people = ['Jack', 'Ben', 'Nick', 'Bob', 'Jane', 'Pall', 'Jim', 'Justin', 'Brenda', 'Opra'];
+// for (let i=0; i < people.length; i++ ) {
+//   document.write(people[i]);
+// };
+
+// for (i of people) {
+//   // console.log(i)
+//   document.write(i);
+// };
+while (let i=0; i < people.length;) {
+  document.write(i);
+  i++;
+};
